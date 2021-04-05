6910c788f79e96391db04fb58f6f47e955da464e
marinoL98 <62379689+marinoL98@users.noreply.github.com>
2021-04-05 20:26:45 +0200

    prominia css searchbara doda nove divove u wishlist.css(prominit cemo ga u profil nece se vise zvat wishlist) na klik login izbacuje div za login(ovo cemo i za ostale napravit)

--- wishlist.js
+++ wishlist.js
@@ -6,4 +6,34 @@ $( function() {
     $( "#sortable" ).disableSelection();
   } );
 
+//popup login
+  $("#prvi").click(function(){
+   PopupLogin();
+   });
+
+   function PopupLogin(){
+    $("#loginpopup").fadeIn();
+    $("#loginpopup").css({"visibility":"visible","display":"block"})
+    
+    $("nav,footer,div:not(#loginpopup)").css({ //koristia san nav footer div umisto cili body jer inace ne radi
+      'filter':'blur(13px)',
+   });
+
+   }
+
+   $("#exitbtn").click(function(){
+    hidepopuplogin();
+   });
+
+
+   function hidepopuplogin(){
+    $("#loginpopup").fadeOut();
+    $("loginpopup").css({"visibility":"hidden","display":"none"});
+    $("nav,footer,div:not(#loginpopup)").css({ //koristia san nav footer div umisto cili body jer inace ne radi
+      'filter':'blur(0px)',
+   });
+
+
+    }
+
 });
