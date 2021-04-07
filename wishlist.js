d783fa81fdd255db4b49814e228fbbb50e562859
marinoL98 <62379689+marinoL98@users.noreply.github.com>
2021-04-07 19:12:22 +0200

    isto doda popup za povratne informacije

--- wishlist.js
+++ wishlist.js
@@ -28,12 +28,45 @@ $( function() {
 
    function hidepopuplogin(){
     $("#loginpopup").fadeOut();
-    $("loginpopup").css({"visibility":"hidden","display":"none"});
+    $("#loginpopup").css({"visibility":"hidden","display":"none"});
     $("nav,footer,div:not(#loginpopup)").css({ //koristia san nav footer div umisto cili body jer inace ne radi
       'filter':'blur(0px)',
    });
 
 
     }
+
+
+    //povratne info popup
+
+    $("#drugi").click(function(){
+      PovPopup();
+      });
+   
+      function PovPopup(){
+       $("#povratnepopup").fadeIn();
+       $("#povratnepopup").css({"visibility":"visible","display":"block"})
+       
+       $("nav,footer,div:not(#povratnepopup)").css({ //koristia san nav footer div umisto cili body jer inace ne radi
+         'filter':'blur(13px)',
+      });
+   
+      }
+   
+      $("#exitbtn2").click(function(){
+       hidepopuppov();
+      });
+   
+   
+      function hidepopuppov(){
+       $("#povratnepopup").fadeOut();
+       $("#povratnepopup").css({"visibility":"hidden","display":"none"});
+       $("nav,footer,div:not(#povratnepopup)").css({ //koristia san nav footer div umisto cili body jer inace ne radi
+         'filter':'blur(0px)',
+      });
+   
+   
+       }
+   
 
 });
